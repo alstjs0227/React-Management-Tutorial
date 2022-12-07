a836d55f22bf418a20eb601dadd28db8cfcb8e9c
minsun <alstjsdl0413@naver.com>
2022-12-07 21:56:41 +0900

    Apply Material-ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Customer from './Components/Customer';
+import Table from '@material-ui/core/Table';
+import Paper from '@material-ui/core/Paper';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
 
-const customers = [{
+const styles = theme => ({
+  root: {
+    with: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  talbe: {
+    minwidth: 1080
+  }
+})
+
+const customers = [
+{
   'id': 1,
   'image':'https://placeimg.com/64/64/1',
   'name': '안길성',
@@ -25,51 +44,29 @@ const customers = [{
   'birthday': '980312',
   'gender': '남자',
   'job': '대학원생'
-}]
+}
+]
+
 function App() {
   return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-            <Customer
-              key={c.id}
-              mage = {c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-              />
-          )
-        })
-      }
-      <Customer
-      id = {customers[0].id}
-      image = {customers[0].image}
-      name={customers[0].name}
-      birthday={customers[0].birthday}
-      gender={customers[0].gender}
-      job={customers[0].job}
-    />
-    <Customer
-      id = {customers[1].id}
-      image = {customers[1].image}
-      name={customers[1].name}
-      birthday={customers[1].birthday}
-      gender={customers[1].gender}
-      job={customers[1].job}
-    />
-    <Customer
-      id = {customers[2].id}
-      image = {customers[2].image}
-      name={customers[2].name}
-      birthday={customers[2].birthday}
-      gender={customers[2].gender}
-      job={customers[2].job}
-    />
-    </div>
-    
-  );
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {customers.map(c => {return (<Customer key={c.id} image = {c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
+          </TableBody>
+        </Table>
+      </Paper>
+      
+    );
 }
 
 export default App;
